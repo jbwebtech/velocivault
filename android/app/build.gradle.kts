0683a1c2208697b9ecb42ebf077cb11344911c5e
jbwebtech <justinwbianchi@gmail.com>
2024-03-03 13:24:52 -0500

    Add fuel and gson dependencies

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -46,7 +46,13 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+
+    implementation(libs.fuel)
+    implementation(libs.fuel.gson)
+    implementation(libs.gson)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
