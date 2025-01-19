a2cb0f8496a38cffe54425b2146ed07a44929064
jbwebtech <justinwbianchi@gmail.com>
2025-01-19 17:46:23 -0500

    TODO

--- android/app/src/main/java/com/jbwebtech/velocivault/FirstFragment.kt
+++ android/app/src/main/java/com/jbwebtech/velocivault/FirstFragment.kt
@@ -31,6 +31,7 @@ class FirstFragment : Fragment() {
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and onDestroyView.
+    //TODO: switching views too quickly can throw a NullPointerException from onViewCreated
     private val binding get() = _binding!!
 
     override fun onCreateView(
