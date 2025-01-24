111865e44794b57b33b917c2663b48ea123461ef
jbwebtech <justinwbianchi@gmail.com>
2025-01-24 13:26:18 -0500

    Load mock data into Library and refactor getLibrary() to return preloaded instance

--- api/repository.js
+++ api/repository.js
@@ -1,16 +1,20 @@
 const fs = require('fs');
 
-const { Library, Word, Noun, Verb, Adverb, Adjective } = require('./lib');
+const mockData = require('./mock-datastore');
+const { Library } = require('./lib');
 
+const library = new Library();
+
+console.log('Loading mock data...');
+library.addWords(mockData.getNouns());
+library.addWords(mockData.getVerbs());
+library.addWords(mockData.getAdverbs());
+library.addWords(mockData.getAdjectives());
+library.addSpecialCharacters(mockData.getSpecialCharacters());
+console.log('Mock data loaded.');
 
 const getLibrary = () => {
-    const lib = new Library();
-    fs.readdirSync('./data/nouns').forEach(filename => {
-        let fileData = fs.readFileSync(`./data/nouns/${filename}`);
-        let json = JSON.parse(fileData);
-        lib.addWords(json);
-    });
-    return lib;
+    return library;
 }
 
 module.exports = {
