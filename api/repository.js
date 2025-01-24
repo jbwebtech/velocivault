db380f66c40e50fe7c4d62c1a0fd01321d967750
jbwebtech <justinwbianchi@gmail.com>
2025-01-24 13:27:39 -0500

    Remove unnecessary file system import and console log from repository.js

--- api/repository.js
+++ api/repository.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
-
 const mockData = require('./mock-datastore');
 const { Library } = require('./lib');
 
 const library = new Library();
 
-console.log('Loading mock data...');
 library.addWords(mockData.getNouns());
 library.addWords(mockData.getVerbs());
 library.addWords(mockData.getAdverbs());
